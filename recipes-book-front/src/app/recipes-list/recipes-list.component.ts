0e605a4671e4e71e208c59fae8759eac15d6503a
Captain Storm <CaptainStorm21@users.noreply.github.com>
2022-05-09 17:20:01 -0700

    Chapter 4: Fetching Data as Streams - part 1

--- recipes-book-front/src/app/recipes-list/recipes-list.component.ts
+++ recipes-book-front/src/app/recipes-list/recipes-list.component.ts
@@ -1,21 +1,28 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
+import { Observable, Subject, takeUntil } from 'rxjs';
+import { Recipe } from '../core/model/recipe.model';
 import { RecipesService } from '../core/services/recipes.service';
-
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.scss'],
+  styleUrls: ['./recipes-list.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RecipesListComponent implements OnInit {
-
+  recipes!: Recipe[];
   recipes$ = this.service.recipes$;
-
   constructor(private service: RecipesService) { }
+  destroy$ = new Subject<void>();
 
   ngOnInit(): void {
+    this.service.getRecipes().pipe(
+      takeUntil(this.destroy$)).
+      subscribe(result => {
+        this.recipes = result;
+      });
   }
-
-
-
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
